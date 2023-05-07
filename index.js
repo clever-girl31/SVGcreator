68abd6f1f99da2fb08c5b320294a3f0db6600567
clever-girl31 <johnmseybold@gmail.com>
2023-05-07 16:41:41 -0500

    created prompts

--- index.js
+++ index.js
@@ -1,2 +1,40 @@
 // TODO: Include a class for each -- Triangle, Circle, Square.
 // Todo: Include test files for each class ^^^
+
+const inquirer = require('inquirer')
+const jest = require('jest')
+const fs = require('fs')
+
+inquirer
+  .prompt([ {
+    type: 'input',
+    name: 'txt',
+    message: 'Enter your logo text: (up to three characters)'
+  },
+   {
+    type: 'input',
+    name: 'txtColor',
+    message: 'Choose text color: (enter keyword or hexadecimal value',
+  },
+  {
+    type: 'list',
+    name: 'shape',
+    message: 'Choose a shape:',
+    choices: ['Circle', 'Square', 'Triangle']
+  },
+  {
+    type: 'input',
+    name: 'shapeColor',
+    message: 'Choose a fill color: (enter keyword or hexadecimal val',
+  }
+  ])
+  .then((answers) => {
+      console.log(`${answers.txt}, ${answers.txtColor}, ${answers.shape}, ${answers.shapeColor}`)
+  })
+  // .catch((error) => {
+  //   if (error.isTtyError) {
+  //     console.log('no')
+  //   } else {
+  //     console.log('yes')
+  //   }
+  // });
