203ff3e5ed4f6b3dc7d04e14b3a013b0a04d1c45
clever-girl31 <johnmseybold@gmail.com>
2023-05-09 21:45:55 -0500

    finished shape classes, moved to shapes.js, added require()

--- index.js
+++ index.js
@@ -4,18 +4,7 @@
 const inquirer = require('inquirer')
 const jest = require('jest')
 const fs = require('fs')
-
-class Square {
-  constructor(shapeColor, txtColor, txt) {
-  this.svg = `<svg width = "230" height = "230" viewBox = "0 0 230 230" >
-  <rect x="0" y="0" width="230" height="230" fill="${shapeColor}"></rect>
-  <text x="115" y="115" text-anchor="middle" fill="${txtColor}">${txt}</text>
-</svg>`
-}}
-
-const shapeOptions = {
-  'Square': Square
-}
+const { shapeOptions } = require('./lib/shapes.js')
 
 inquirer
   .prompt([ {
@@ -44,7 +33,7 @@ inquirer
     const { shape, shapeColor, txtColor, txt } = answers;
     const InsertShapeClassHere = shapeOptions[shape]
     const logo = new InsertShapeClassHere(shapeColor, txtColor, txt)
-    fs.writeFile('generatedlogo.svg', logo.svg, (err) => {
+    fs.writeFile('./output/generatedlogo.svg', logo.svg, (err) => {
       if (err) throw (err);
       console.log ("Logo saved!")
     })
